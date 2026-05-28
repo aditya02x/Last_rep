8dd30734d9a9a5b7b2e4cad8c1b892125a20b3e9
Aditya Raj <adityaraj34@icloud.com>
2026-05-29 00:04:33 +0530

    register

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
-
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 export const register = async (req,res)=>{
     try {
         const {name,email,password}= req.body;
@@ -11,12 +12,28 @@ export const register = async (req,res)=>{
             return res.status(400).json({message:'User already exists'});
         }
 
-        const user = new User({name,email,password});
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const user = new User({name,email,password:hashedPassword});
         await user.save();
         res.status(201).json({message:'User registered successfully'});
     }
     catch (error) {
         console.error('Error in register controller:', error);
         res.status(500).json({message:'Server error'});
+    }
+}
+
+export const login = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message:'Please fill all fields'});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:'Invalid credentials'});
+        }
+
     }
 }
