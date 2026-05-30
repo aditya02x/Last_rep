f45ceba0c358ba554d91bac1cf432fe23593283e
Aditya Raj <adityaraj34@icloud.com>
2026-05-31 00:06:51 +0530

    user info

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -13,7 +13,52 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    age:{
+        type:Number,
+        min:0,
+        max:120
+        
+
+    },
+    gender:{
+        type:String,
+        enum:['Male','Female','Other']
+        },
+    height:{
+        type:Number,
+        min:0
+    },
+    currentWeight:{
+        type:Number,
+        min:0
+    },
+    goalWeight:{
+        type:Number,
+        min:0
+    },
+      goal: {
+    type: String,
+    enum: [
+      "muscle_gain",
+      "fat_loss",
+      "maintenance"
+    ]
+  },
+
+  activityLevel: {
+    type: String,
+    enum: [
+      "beginner",
+      "intermediate",
+      "advanced"
+    ]
+  },
+  isOnboarded: {
+    type: Boolean,
+    default: false
+  }
+
 },{timestamps:true});
 
 export default mongoose.model('User',userSchema);
