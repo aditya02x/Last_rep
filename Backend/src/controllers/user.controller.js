cbc3b5ebb1e5b1367a96d4e8e6d868f0df54b5c6
Aditya Raj <adityaraj34@icloud.com>
2026-05-31 19:57:08 +0530

    updated user

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -7,10 +7,42 @@ export const getProfile = async (req,res)=>{
         if(!user){
             return res.status(404).json({message:'User not found'});
         }
-        
+
         res.json(user);
     } catch (error) {    
             console.error(error);
+        res.status(500).json({message:'Server error'});
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try{
+        const {age ,gender, height, currentWeight, goalWeight, goal, activityLevel} = req.body;
+        const user = await User.findById(req.user._id);
+
+        if(!user){
+            return res.status(404).json({message:'User not found'});
+
+        }
+        user.age = age ?? user.age;
+        user.gender = gender ?? user.gender;
+        user.height = height ?? user.height;
+        user.currentWeight = currentWeight ?? user.currentWeight;
+        user.goalWeight = goalWeight ?? user.goalWeight;
+        user.goal = goal ?? user.goal;
+        user.activityLevel = activityLevel ?? user.activityLevel;
+
+        user.isOnboarded = true;
+
+        await user.save();
+        const updatedUser = await User.findById(req.user._id).select('-password');
+         res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+    } catch (error) {
+        console.error(error);
         res.status(500).json({message:'Server error'});
     }
 }
