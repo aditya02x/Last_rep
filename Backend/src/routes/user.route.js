1b9aa73f9326310070a72dd2a44610f81679a062
Aditya Raj <adityaraj34@icloud.com>
2026-05-31 20:17:38 +0530

    changes

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getProfile, updateProfile } from '../controllers/user.controller.js';
+import protect from '../middleware/auth.middleware.js';
 const router = express.Router();
 
-router.get("/profile",getProfile);
+router.get("/profile", protect, getProfile);
 router.patch("/profile",updateProfile);
 
 export default router;
