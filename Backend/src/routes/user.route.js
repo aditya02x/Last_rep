1fa23f41cf4d3f962a4416ff5497b0f4c53ac342
Aditya Raj <adityaraj34@icloud.com>
2026-05-31 19:58:58 +0530

    updated user route

---
+++ Backend/src/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getProfile, updateProfile } from '../controllers/user.controller.js';
+const router = express.Router();
+
+router.get("/profile",getProfile);
+router.patch("/profile",updateProfile);
+
+export default router;
