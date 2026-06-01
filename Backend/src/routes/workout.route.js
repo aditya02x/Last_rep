778ba3245d1a4477aab7ed76f4258616e689e9bf
Aditya Raj <adityaraj34@icloud.com>
2026-06-01 13:19:29 +0530

    changes

--- Backend/src/routes/workout.route.js
+++ Backend/src/routes/workout.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { craeteWorkout } from '../controllers/workout.controller.js';
+import { createWorkout } from '../controllers/workout.controller.js';
 import protect from '../middleware/auth.middleware.js';
 const router = express.Router();
-router.post("/",protect,craeteWorkout);
+router.post("/",protect,createWorkout);
 
 export default router;
