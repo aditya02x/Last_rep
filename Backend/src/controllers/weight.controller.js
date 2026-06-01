d661679555a43cf868aa3654de130208dcc03777
Aditya Raj <adityaraj34@icloud.com>
2026-06-02 01:58:08 +0530

    weight controller

---
+++ Backend/src/controllers/weight.controller.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import  Weight from '../models/weight.model.js';
+export const addWeight = async (req,res)=>{
+    try{
+        const {weight} = req.body;
+        if(!weight){
+            return res.status(400).json({message:'Weight is required'})
+        }
+
+        const weightEntry = new Weight({
+            user:req.user._id,
+            weight,
+        });
+
+        await weightEntry.save();
+    }
+    catch(error){
+        console.error('Error in addWeight controller:', error);
+        res.status(500).json({message:'Server error'});
+    }
+}
