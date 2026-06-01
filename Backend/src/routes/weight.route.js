8be1d871976eacb3e252a78e9c83613bc26da91a
Aditya Raj <adityaraj34@icloud.com>
2026-06-02 02:00:34 +0530

    weight routes

--- Backend/src/routes/weight.route.js
+++ Backend/src/routes/weight.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { addWeight } from '../controllers/weight.controller.js';
+import protect from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
-router.post('/',addWeight)
+router.post('/', protect, addWeight)
 export default router;
