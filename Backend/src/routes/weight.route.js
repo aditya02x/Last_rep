46f61fc7620214bfe0ba232e3b80b2c04344e5d7
Aditya Raj <adityaraj34@icloud.com>
2026-06-02 02:08:57 +0530

    whis route

--- Backend/src/routes/weight.route.js
+++ Backend/src/routes/weight.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addWeight } from '../controllers/weight.controller.js';
+import { addWeight, getWeightsHistory } from '../controllers/weight.controller.js';
 import protect from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
 router.post('/', protect, addWeight)
+router.get('/',protect,getWeightsHistory)
 export default router;
