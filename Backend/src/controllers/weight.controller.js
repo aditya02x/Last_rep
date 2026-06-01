30db3a2553a3ea8f0b32d657b20570facf4a0791
Aditya Raj <adityaraj34@icloud.com>
2026-06-02 02:05:00 +0530

    weight routess

--- Backend/src/controllers/weight.controller.js
+++ Backend/src/controllers/weight.controller.js
@@ -3,22 +3,32 @@ import  Weight from '../models/weight.model.js';
 export const addWeight = async (req,res)=>{
     try{
         const {weight} = req.body;
+
         if(!weight){
-            return res.status(400).json({message:'Weight is required'})
+            return res.status(400).json({
+                message:'Weight is required'
+            });
         }
 
         const weightEntry = new Weight({
-            sucess:true,
             user:req.user._id,
             weight,
         });
 
         await weightEntry.save();
-    }
-    catch(error){
+
+        res.status(201).json({
+            success:true,
+            message:'Weight logged successfully',
+            weightEntry
+        });
+
+    } catch(error){
         console.error('Error in addWeight controller:', error);
+
         res.status(500).json({
             success:false,
-            message:'Server error'});
+            message:'Server error'
+        });
     }
 }
