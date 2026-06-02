8ae13c2f6ba2b104debd645bad96e44dce3de782
Aditya Raj <adityaraj34@icloud.com>
2026-06-03 02:36:21 +0530

    dashboard jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useEffect } from "react";
+import { getDashboard } from "../services/dashboardService";
 
-const Dashboard = () => {
+function Dashboard() {
+  useEffect(() => {
+    const fetchDashboard = async () => {
+      try {
+        const data = await getDashboard();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDashboard();
+  }, []);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="min-h-screen bg-[#0f172a] text-white p-6">
+      <h1 className="text-3xl font-bold">
+        Dashboard
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
