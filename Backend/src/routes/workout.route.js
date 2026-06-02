b7a263f8208344aaff828cdc9dc872349b133fba
Aditya Raj <adityaraj34@icloud.com>
2026-06-02 23:27:25 +0530

    dashboard testds

--- Backend/src/routes/workout.route.js
+++ Backend/src/routes/workout.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createWorkout ,getWorkouts  , getWorkoutLastSession} from '../controllers/workout.controller.js';
+import { createWorkout ,getWorkouts  , getWorkoutLastSession , getWorkoutById} from '../controllers/workout.controller.js';
 import protect from '../middleware/auth.middleware.js';
 const router = express.Router();
 router.post("/",protect,createWorkout);
 router.get("/" ,protect,getWorkouts);
 router.get("/last-session/:exerciseName", protect, getWorkoutLastSession);
+router.get("/:id", protect, getWorkoutById);
 
 export default router;
