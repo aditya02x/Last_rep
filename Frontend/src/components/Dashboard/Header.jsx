6e5764ff8b7d77d81e7aacbc88007ae4b285a741
Aditya Raj <adityaraj34@icloud.com>
2026-06-03 23:39:05 +0530

    header name

---
+++ Frontend/src/components/Dashboard/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    const today = new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
