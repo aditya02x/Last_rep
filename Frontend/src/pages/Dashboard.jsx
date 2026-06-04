92390a3fbf23789c6703366ff75723221fe60803
Aditya Raj <adityaraj34@icloud.com>
2026-06-04 23:54:24 +0530

    goal progressss ss

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../components/Dashboard/Header' 
 import WeightCard from '../components/Dashboard/Weight' // adjust path if needed
+import GoalProgressCard from '../components/Dashboard/GoalProgress' // adjust path if needed
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-[#000000] p-6">
       <Header />
       <WeightCard />
+      <GoalProgressCard
+  currentWeight={dashboard.currentWeight}
+  goalWeight={dashboard.goalWeight}
+/>
       {/* rest of your dashboard content goes here */}
     </div>
   )
