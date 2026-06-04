4e6af48578eb81430b7a2bf3c98656026f38a138
Aditya Raj <adityaraj34@icloud.com>
2026-06-05 00:08:45 +0530

    last workout cardss

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../components/Dashboard/Header' 
 import WeightCard from '../components/Dashboard/Weight' // adjust path if needed
  // adjust path if needed
-
+import LastWorkout from '../components/Dashboard/LastWorkout' // adjust path if needed
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-[#000000] p-6">
       <Header />
       <WeightCard />
+      <LastWorkout />
 
 
       {/* rest of your dashboard content goes here */}
