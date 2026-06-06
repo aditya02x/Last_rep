23457cb069c6bcc1d0be1b51bd7f1569c1ecd81e
Aditya Raj <adityaraj34@icloud.com>
2026-06-06 23:57:13 +0530

    weight graph add changesssss

--- Frontend/src/components/Dashboard/Footer.jsx
+++ Frontend/src/components/Dashboard/Footer.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Home, Dumbbell, TrendingUp, User } from 'lucide-react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-const Footer = () => {
-  const navigate = useNavigate();
   const location = useLocation();
 
   const tabs = [
